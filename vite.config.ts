66ee0d19c3785335d624ed4f7f4b7dcbbc5610f3
Privilege Mendes <privilegemendes@gmail.com>
2024-11-08 21:48:41 +0100

    added eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import path from "path";
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src"),
+      '~': path.resolve(__dirname, './src'),
     },
   },
   server: {
     port: 8000,
   },
-})
+});
