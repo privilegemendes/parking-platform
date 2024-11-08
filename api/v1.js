8cbc4f2af4295c70f2cb91a1a01e9eacdf9690d6
Privilege Mendes <privilegemendes@gmail.com>
2024-11-09 00:55:02 +0100

    refactor: removed middlware

--- api/v1.js
+++
@@ -1,7 +0,0 @@
-// api/v1.js
-import { createProxyMiddleware } from "http-proxy-middleware";
-
-export default createProxyMiddleware({
-  target: "https://parkdemeer-afde952e3fef.herokuapp.com",
-  changeOrigin: true,
-});
