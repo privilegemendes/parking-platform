3b85b67880111eacfd3969ffbe5239cce54522bb
Privilege Mendes <privilegemendes@gmail.com>
2024-11-09 00:58:25 +0100

    feat: added tabs to dashboard

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -1,13 +1,30 @@
 import { useAuth } from "~/contexts/authentication-provider";
 import { Button } from "~/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { Logo } from "~/components/ui/logo";
 
 export default function DashboardPage() {
   const { logout } = useAuth();
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-      <div className="flex flex-1 justify-end">
-        <Button onClick={logout}>Logout</Button>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <Logo />
+        <div className="flex flex-1 justify-end">
+          <Button onClick={logout}>Logout</Button>
+        </div>
+      </header>
+      <div className="flex flex-col px-4 mx-auto">
+        <Tabs defaultValue="overview" className="space-y-4 mt-4">
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="parking-logs">Parking Logs</TabsTrigger>
+            <TabsTrigger value="financials">Financials</TabsTrigger>
+          </TabsList>
+          <TabsContent value="overview" className="space-y-4"></TabsContent>
+          <TabsContent value="parking-logs" className="space-y-4"></TabsContent>
+          <TabsContent value="financials" className="space-y-4"></TabsContent>
+        </Tabs>
       </div>
-    </header>
+    </>
   );
 }
