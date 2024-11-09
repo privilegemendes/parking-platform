c9cfaf941aec2dfd07995e7d30a808fe826909d5
Privilege Mendes <privilegemendes@gmail.com>
2024-11-09 10:12:30 +0100

    feat: added data table template

---
+++ src/components/data-table/data-table-toolbar.tsx
@@ -0,0 +1,67 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Table } from "@tanstack/react-table";
+import { DataTableFacetedFilter } from "~/components/data-table/data-table-facted-filter";
+import { Button } from "~/components/ui/button";
+import { DataTableViewOptions } from "~/components/data-table/data-table-view-options";
+import {
+  parkingSessionsStatuses,
+  parkingSpacesTypes,
+  vehicleTypes,
+} from "~/components/parking-sessions/parking-sessions-columns";
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTableToolbar<TData>({
+  table,
+}: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 items-center space-x-2">
+        {/*<Input*/}
+        {/*  placeholder="Filter tasks..."*/}
+        {/*  value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}*/}
+        {/*  onChange={(event) =>*/}
+        {/*    table.getColumn("title")?.setFilterValue(event.target.value)*/}
+        {/*  }*/}
+        {/*  className="h-8 w-[150px] lg:w-[250px]"*/}
+        {/*/>*/}
+        {table.getColumn("parkingSpaceId") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("parkingSpaceId")}
+            title="Parking Space"
+            options={parkingSpacesTypes}
+          />
+        )}
+        {table.getColumn("isSessionEnded") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("isSessionEnded")}
+            title="Status"
+            options={parkingSessionsStatuses}
+          />
+        )}
+        {table.getColumn("vehicleType") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("vehicleType")}
+            title="Vehicle Type"
+            options={vehicleTypes}
+          />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <Cross2Icon className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <DataTableViewOptions table={table} />
+    </div>
+  );
+}
