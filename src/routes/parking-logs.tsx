330fffb91c1a32f56d463c21730da7ec5aeebffe
Privilege Mendes <privilegemendes@gmail.com>
2024-11-13 17:26:28 +0100

    feat: added redirect to login for parking logs

--- src/routes/parking-logs.tsx
+++ src/routes/parking-logs.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { ParkingSessionsTable } from "~/components/parking-sessions/parking-sessions-table";
 
 export const Route = createFileRoute("/parking-logs")({
+  beforeLoad: async ({ context }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/login",
+      });
+    }
+  },
   component: ParkingLog,
 });
 
